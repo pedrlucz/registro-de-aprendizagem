9d3d3461eb8c470ae8b87e4646bc5e8b38e35cc5
pedrlucz <pedrlucz7@gmail.com>
2024-12-30 22:34:39 -0300

    só adicionando os blocks e html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views # da mesma pasta que eu to, eu quero importar tal coisa, o .
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = 'index'),
 ]
 
 
